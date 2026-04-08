c01608d8214aac3d483c5d8c5660025b57cdd1b2
stew3254 <16154582+stew3254@users.noreply.github.com>
2026-04-07 21:27:51 -0400

    Add outputs back in

--- terraform/setup/outputs.tf
+++ terraform/setup/outputs.tf
@@ -1,15 +1,15 @@
-# output "firewall" {
-#   value = hcloud_firewall.valheim
-# }
+output "firewall" {
+  value = hcloud_firewall.valheim
+}
 
-# output "ssh_keys" {
-#   value = local.key_names
-# }
+output "ssh_keys" {
+  value = local.key_names
+}
 
 output "all_ssh_keys" {
   value = data.hcloud_ssh_keys.all.ssh_keys
 }
 
-# output "new_ssh_keys" {
-#   value = local.new_ssh_keys
-# }
+output "new_ssh_keys" {
+  value = local.new_ssh_keys
+}
